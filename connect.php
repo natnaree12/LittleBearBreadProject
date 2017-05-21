5a173df78b273a077f8c9650782b24350d1d3cf8
natnaree12 <natnaree.nat12@gmail.com>
2017-05-21 15:49:47 +0700

    connect.php (version 2)

--- connect.php
+++ connect.php
@@ -1,20 +1,32 @@
 <?php
-$user = 'root';
-$password = 'root';
-$db = 'LittleBearBread';
-$host = 'localhost';
-$port = 8889;
+// $user = 'root';
+// $password = 'root';
+// $db = 'LittleBearBread';
+// $host = 'localhost';
+// $port = 8889;
 
-$conn = mysql_connect(
-   "$host:$port", 
-   $user, 
-   $password,
-   $db
-);
-$db_selected = mysql_select_db(
-   $db, 
-   $link
-);
+// $conn = mysql_connect(
+//    "$host:$port", 
+//    $user, 
+//    $password,
+//    $db
+// );
+// $db_selected = mysql_select_db(
+//    $db, 
+//    $link
+// );
+
+$servername = "localhost:8889";
+$username = "root";
+$password = "root";
+$dbname = "LittleBearBread";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
 // if ($link->connect_error) {
 //     die("Connection failed: " . $conn->connect_error);
